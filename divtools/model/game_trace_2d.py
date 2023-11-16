db1007d720bf8dcf769a39d71e37af5fe3eeca7f
Satori31022809947 <3102809947@qq.com>
2023-11-16 14:01:13 +0800

    add game position trace model

--- divtools/model/game_trace_2d.py
+++ divtools/model/game_trace_2d.py
@@ -1,6 +1,6 @@
 
 from divtools.common.model import Model
-from typing import List,Tuple
+from typing import List,Tuple,Optional
 
 class GameOperationSequence(Model):
     operations: List[Tuple[float,int]]
@@ -23,3 +23,42 @@ class GameOperationSequence(Model):
             for y in range(m):
                 dp[x+1][y+1]=min(dp[x][y+1]+1,dp[x+1][y]+1,dp[x][y]+(0 if obj1.operations[x][1]==obj2.operations[y][1] else 1))
         return dp[n][m]
+    
+
+class Position():
+    pass
+
+class Position2D(Position):
+    x,y: float
+    def __init__(self,position) -> None:
+        self.x = position[0]
+        self.y = position[1]
+    
+    
+class GameTrace(Model):
+    position_type: type
+    positions: List[Position]
+    
+    def __init__(self,position_type,positions:Optional[List]=[]):
+        if not issubclass(position_type, Position):
+            raise ValueError("Invalid type. It should be a subclass of Position.")
+        self.position_type = position_type
+        self.positions = []
+        for position in positions:
+            self.positions.append(position_type(position))
+
+
+
+class GameTraceWithTime(Model):
+    position_type: type
+    positions: List[Tuple[float,Position]]
+    
+    def __init__(self,position_type,positions:Optional[List[Tuple]]=[]):
+        if not issubclass(position_type, Position):
+            raise ValueError("Invalid type. It should be a subclass of Position.")
+        self.position_type = position_type
+        self.positions = []
+        for position in positions:
+            self.positions.append((position[0],position_type(position[1])))
+
+        self.positions=sorted(self.positions)
